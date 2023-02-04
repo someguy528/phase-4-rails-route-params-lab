ce38e74720d50a10cd511ff05157e1b52d334368
Brandon Castro <castroyou999@gmail.com>
2023-02-04 00:59:27 -0500

    commit tree 954edeba1ac9fc8db5bcf40353d5ca8fe421c994

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,13 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    students = students.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:name]}", "%#{params[:name]}") if params[:name]
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
